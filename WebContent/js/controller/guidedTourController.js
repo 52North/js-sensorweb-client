10700385cadd1bb7218a6b191e6b994f233a7e79
janschulte <j.schulte@52north.org>
2014-04-23 14:35:49 +0200

    update the tour guide

--- WebContent/js/controller/guidedTourController.js
+++ WebContent/js/controller/guidedTourController.js
@@ -27,6 +27,23 @@
  * Public License for more details.
  */
 var GuidedTourController = (function() {
+	
+	var timeseriesAdd = function(evt, ts) {
+		if (!ts.hasData() && ts.getLastValue().timestamp) {
+			EventManager.publish("time:end:change", ts.getLastValue().timestamp);
+			return; 
+		};
+		EventManager.unsubscribe("timeseries:data:loadfinished", timeseriesAdd);
+		this.showNext();
+	};
+	
+	var stationLoaded = function() {
+			EventManager.unsubscribe("map:stationLoaded", stationLoaded);
+			setTimeout($.proxy(function() {
+				this.showNext();
+			}, this), 200);
+	};
+	
 	/*
 	 * every step has the following options:
 	 * 
@@ -41,8 +58,7 @@ var GuidedTourController = (function() {
 				initStep : function() {
 					Pages.navigateToChart();
 				}
-			},
-			{
+			},{
 				anchor : '[data-target="#map"]',
 				title : 'Go to the map',
 				text : 'Here we switch the view to get a map.',
@@ -50,76 +66,93 @@ var GuidedTourController = (function() {
 				initStep : function() {
 					Pages.navigateToChart();
 				}
-			},
-			{
+			},{
 				anchor : '.navbar-header.map',
 				title : 'Map view',
 				text : 'This is the map view. In the map you can see markers or markergroups.',
 				initStep : function() {
 					Pages.navigateToMap();
 				}
-			},
-			{
+			},{
 				anchor : '[data-action="provider"]',
 				title : 'Change Provider',
 				text : 'Here you can select another timeseries provider.',
 				arrow : true,
 				initStep : function() {
 				}
-			},
-			{
+			},{
 				anchor : '[data-action="locate"]',
 				title : 'Show location',
 				text : 'And here you can locate your device on the map.',
 				arrow : true,
 				initStep : function() {
 				}
-			},
-			{
+			},{
 				anchor : '[data-action="listSelection"]',
 				title : 'List selection',
 				text : 'Here you can select a timeseries out of ordered lists.',
 				arrow : true,
 				initStep : function() {
 				}
-			},
-			{
+			},{
 				anchor : '.navbar-header.map',
 				title : 'Select a station',
 				text : 'Please select now a station on the map.',
 				next: false,
 				initStep : function(context) {
-					EventManager.subscribe("map:stationLoaded", $.proxy(
-							function() {
-								EventManager.unsubscribe("map:stationLoaded");
-								setTimeout($.proxy(function() {
-									this.showNext();
-								}, this), 200);
-							}, context));
+					EventManager.subscribe("map:stationLoaded", $.proxy(stationLoaded, context)); 
 				}
-			},
-			{
+			},{
 				anchor : '.tsItem input',
 				title : 'Select timeseries',
 				text : 'Select this checkbox. If there is only one timeseries for this station, the checkbox is already checked. Now you can go on with the "OK" button to load the timeseries.',
 				previous : false,
 				next : false,
 				arrow : true,
-				initStep : function() {
+				initStep : function(context) {
+					EventManager.subscribe("timeseries:data:loadfinished", $.proxy(timeseriesAdd, context));
 				}
-			},
-			{
-				anchor : '',
-				title : 'Select timeseries',
-				text : 'Select this checkbox. If there is only one timeseries for this station, the checkbox is already checked. Now you can go on with the "OK" button to load the timeseries.',
+			},{
+				anchor : '.legend-entry',
+				title : 'Legend entry',
+				text : 'Here you see the added time series. You can delete, locate or modify the style of the time series here.',
+				arrow : true,
+				previous : false,
+				initStep : function() {
+					Pages.toggleLegend(true);
+				}
+			},{
+				anchor : '.navbar-header.chart',
+				title : 'Chart',
+				text : 'This is the chart of the selected time series.',
+				initStep : function() {
+					Pages.toggleLegend(false);
+				}
+			},{
+				anchor : '.btn-group.timeSelection',
+				title : 'Change time',
+				text : 'Here you can change the time extent for your selected time series.',
 				arrow : true,
 				initStep : function() {
 				}
-			} ];
+			},{
+				anchor : '[data-action="dataTable"]',
+				title : 'Table View',
+				text : 'Here you get a table of the raw data values to your selected time series.',
+				arrow : true,
+				initStep : function() {
+				}
+			},{
+				anchor : '.navbar-header.chart',
+				title : 'Finished',
+				text : 'Well done!<br> This client is a product of <a href="http://52north.org" target="_blank">52&deg;North GmbH</a>. You can find the source code on <a href="https://github.com/52North/js-sensorweb-client" target="_blank">GitHub</a>.',
+				initStep : function() {
+				}
+			}];
 
 	var template = '<div class="popover guidedtour"><div class="popover-content"></div></div>';
 	var arrowtemplate = '<div class="popover guidedtour"><div class="arrow"></div><div class="popover-content"></div></div>';
-
+	
 	return {
 		init : function() {
 			// initialize button
@@ -129,7 +162,6 @@ var GuidedTourController = (function() {
 				});
 			});
 			// start by permalink
-			debugger;
 			if (Permalink.getUrlParameter('tour') === "true") {
 				GuidedTourController.start();
 			}
@@ -158,6 +190,7 @@ var GuidedTourController = (function() {
 			this.gtWindow = $(step.anchor + ':first').popover({
 				html : true,
 				template : step.arrow ? arrowtemplate : template,
+				trigger: 'manual',
 				content : Template.createHtml('guidedtour', {
 					title : step.title,
 					text : step.text,
