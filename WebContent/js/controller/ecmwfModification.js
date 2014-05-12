f5d69581681c148fbda60dc022b34102f0163491
janschulte <j.schulte@52north.org>
2014-05-12 14:13:24 +0200

    update ecmwf mod

--- WebContent/js/controller/ecmwfModification.js
+++ WebContent/js/controller/ecmwfModification.js
@@ -47,8 +47,19 @@ Map.openStationWindow = $.proxy(function(id, url) {
 				}
 				ensembleGroups[ensemble].push({
 					'id' : id,
-					'internalId' : TimeSeries.createInternalId(id, url) 
+					'internalId' : tsHelper.createInternalId(id, url) 
 				});
+			});
+			// load result time options
+			$.each(ensembleGroups, function(id, array) {
+				Rest.resulttime(array[0].id, url).done($.proxy(function(result) {
+					$('[data-ensemble="' + id + '"] .resultTimeSelection').empty();
+					var select = $('<select class="form-control"></div>');
+					$.each(result, function(id, elem) {
+						select.append('<option data-time="' + elem + '">' + moment(elem).format(Settings.dateformat) + '</option>');
+					});
+					$('[data-ensemble="' + id + '"] .resultTimeSelection').append(select);
+				}, this));
 			});
 			this.loading(false);
 			Modal.show("station-ecmwf", {
@@ -62,19 +73,23 @@ Map.openStationWindow = $.proxy(function(id, url) {
 			});
 			$('#confirmTimeseriesSelection').on('click', function() {
 				// show loading info
-				if ($('.tsItem').has(':checked').length > 0) {
+				if ($('.tsItem').has('.checkbox :checked').length > 0) {
 					$('.ecmwf-station-loading').show();
-					$.each($('.tsItem').has(':checked'), function(id, elem) {
-						$.each(ensembleGroups[$(this).data('ensemble')], function(idx, elem) {
+					$.each($('.tsItem').has('.checkbox :checked'), function(id, elem) {
+						debugger;
+						var resultTime = $(elem).find('.form-control :selected').data('time');
+						// TODO check the selected result time
+						$.each(ensembleGroups[$(this).data('ensemble')], $.proxy(function(idx, elem) {
 							if (Map.timeseriesCache[elem.internalId] == null) {
 								Rest.timeseries(elem.id, url).done(function(timeseries) {
+									timeseries.setResultTime(resultTime);
 									Map.timeseriesCache[timeseries.getInternalId()] = timeseries;
 									Map.addTimeseries(timeseries);
 								});
 							} else {
 								Map.addTimeseries(Map.timeseriesCache[elem.internalId]);
 							}
-						});
+						}, this));
 					});
 				} else {
 					Modal.hide();
@@ -86,3 +101,31 @@ Map.openStationWindow = $.proxy(function(id, url) {
 		oldOpenStationWindow(id, url);
 	}
 },Map);
+
+Rest.resulttime = function(id, apiUrl, data) {
+	return Rest.request(apiUrl + "timeseries/"
+			+ (id == null ? "" : id), data, function(promise, result) {
+		// TODO update with real request
+		promise.resolve([1399413600000,1399435200000,1399456800000,1399478400000]);
+	});
+};
+
+var oldTimeSeries = TimeSeries;
+
+TimeSeries = function() {
+	console.log('Before');
+	var result = oldTimeSeries.apply(this, arguments);
+	
+	var resultTime;
+	this.setResultTime = function(rt) {
+		resultTime = rt;
+	};
+	
+	this.fetchData = function(timespan, complete) {
+		this.promise = Rest.tsData(this.getTsId(), this.getApiUrl(), timespan, this.getInternalId(), {'resultTime':resultTime});
+		this.promise.done($.proxy(this.fetchedDataFinished, {context:this, complete:complete}));
+		return this.promise;
+	};
+	
+	return result;
+};
